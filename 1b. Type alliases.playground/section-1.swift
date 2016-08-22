76e6e72ddd26b431186dcd3d6c6b9bbe318b4be7
happyyi <2060842031@qq.com>
2016-08-22 13:13:42 +0800

    1b 类型别名翻译完毕

--- 1b. Type alliases.playground/section-1.swift
+++ 1b. Type alliases.playground/section-1.swift
@@ -4,6 +4,7 @@
 // * Type Aliases allow you to provide a different name for types,
 //   similar to 'typedef' in C.
 // ------------------------------------------------------------------------------------------------
+// 类型别名，类似于C语言的宏定义
 
 // Create an alias for UInt16 called "AudioSample"
 typealias AudioSample = UInt16
@@ -12,6 +13,7 @@ typealias AudioSample = UInt16
 var maxAmplituedFound = AudioSample.min
 
 // We can also typealias custom types
+// 类型别名，可用于自定义类型
 struct MySimpleStruct
 {
 	static let a = 99
